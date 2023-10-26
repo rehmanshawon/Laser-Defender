c3f8f6786a2d085f97fad471aa326f2039e107a2
rehmanshawon <meghnadbadh@gmail.com>
2023-10-26 18:57:14 +0600

    initial commint

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    int score;
+
+    static ScoreKeeper instance;
+
+    private void Awake()
+    {
+        ManageSingleton();
+    }
+
+    private void ManageSingleton()
+    {
+        //int instanceCount =  FindObjectsOfType(GetType()).Length;
+        // if(instanceCount > 1)
+        if (instance != null)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void ModifyScore(int value)
+    {
+        score += value;
+        Mathf.Clamp(score, 0, int.MaxValue);
+        Debug.Log(score);
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+    }
+}
